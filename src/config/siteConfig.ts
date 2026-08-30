25d3d43aa55281627c27086daeb3f024f5a86f05
茶清墨刂 <3271235381@qq.com>
2026-08-30 23:50:46 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,8 +6,8 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 export const siteConfig: SiteConfig = {
   title: "茶清墨刂",
   subtitle: "个人网站",
-  siteURL: "https://tea/", // 请替换为你的站点URL，以斜杠结尾
-  siteStartDate: "2026-08-26", // 站点开始运行日期，用于站点统计组件计算运行天数
+  siteURL: "https://teaclearinkii.pages.dev/", // 请替换为你的站点URL，以斜杠结尾
+  siteStartDate: "2026-08-30", // 站点开始运行日期，用于站点统计组件计算运行天数
   timeZone: "Asia/Shanghai", // 文章日期使用的 IANA 时区，可改为 Asia/Tokyo、Europe/Berlin 等
 
   lang: SITE_LANG,
